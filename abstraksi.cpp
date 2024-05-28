e431d10e1267a3c33e3e4a7c87a355969e2915d3
Pannntastic <pannntastic@gmail.com>
2024-05-28 18:50:20 +0700

    class abstraksi

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class abstraksi{
+    private : 
+        string x,y;
+    public : 
+        // method untk mengisi nilai
+        // private untuk member
+        void setxy(string a, string b){
+            x=a;
+            y=b;
+        }
+        //menampilkan nilai
+        void display(){
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
+        }
+};
