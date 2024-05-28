39e77a7e9c6f93e0967126c9f4a7edadd85439ed
Pannntastic <pannntastic@gmail.com>
2024-05-28 19:13:22 +0700

    solve multiwaris menghapus virtual

---
+++ solvemultiwaris.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+    public : 
+        int umur ;
+        orang (int pumur) : umur (pumur){
+            cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+        }
+};
+
+class pekerja : public orang {
+    public : 
+        pekerja(int pumur) : orang (pumur){
+            cout << "Pekerja dibuat\n" << endl;
+        }
+};
+
+class pelajar : virtual public orang {
+    public : 
+        pelajar(int pumur) : orang (pumur){
+            cout << "Pelajar dibuat\n" << endl;
+        }
+};
+
+class budi : public pekerja , public pelajar{
+    public :
+        budi (int pumur) : pekerja (pumur), pelajar (pumur), orang (pumur){ // hal ini dapat d lakukan jika menggunakan virtual
+            cout << "Budi Dibuat\n" << endl;
+        }
+};
+
+int main (){
+    budi a(12);
+}
+
